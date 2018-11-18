c71fe5b04a6de3bb2434174d51635acfd10a64f8
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-11-18 21:49:50 +0000

    Update initial files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /** ********** CONFIGS ********** **/
 
@@ -6,28 +7,85 @@ const imgLoaderSizeLimit = 1024 * 10; // 10kb
 
 /** ********** MAIN ********** **/
 
-module.exports = {
+module.exports = (env, argv) => ({
+  mode: env.prod ? 'production' : 'development',
+
   entry: {
     index: './src/index.tsx',
   },
 
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/static'),
+
+    publicPath: env.prod ? '/static/' : '/',
+    filename: env.prod ? '[name].[contenthash].js' : '[name].js',
+    chunkFilename: env.prod ? '[name].[contenthash].js' : '[name].js',
   },
 
   module: {
     rules: [
+      // TypeScript
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+
+      // SVG
+      {
+        test: /\.(svg)$/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {
+            limit: imgLoaderSizeLimit,
+            noquotes: true,  // Remove quotes around the encoded URL
+          },
+        },
+        exclude: /node_modules/,
+      },
+
+      // Image compression
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: {
+          loader: 'image-webpack-loader',
+        },
+        enforce: 'pre',
+        exclude: /node_modules/,
+      },
     ],
   },
 
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index_prod.html',
+      chunks: ['index', 'vendors', 'runtime~index'],
+      favicon: path.resolve(__dirname, 'src/img/favicon.ico'),
+      template: path.resolve(__dirname, 'src/html/index_prod.html'),
+    }),
+  ],
+
+  optimization: {
+    
   },
 
-  devtool: 'inline-source-map',
-};
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+
+  devtool: env.prod ? null : 'cheap-module-eval-source-map',
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src/html/index_dev.html'),
+    compress: true,
+    port: 8080,
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+    staticOptions: {
+      extensions: ['html'],
+    },
+    hot: true,
+  },
+});
